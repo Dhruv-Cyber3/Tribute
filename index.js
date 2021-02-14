887df03d1a7cdda8942e26c8b838493955318428
Dhruv Choudhary <totallynoobcoder@gmail.com>
2021-02-14 10:45:42 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+
+const app = express();
+
+app.use('/public', express.static('public'));
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs');
+
+
+app.get('/norman_borlaug', (req, res) => {
+    res.render('tributeWebsite');
+})
+
+app.listen(3000, () => {
+    console.log("Server started at 3000");
+})
+
+//comment
